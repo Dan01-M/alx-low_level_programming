8a0be1d121cd3dddd9e88d099bae64f0994b7816
Dan01-M <www.dan56mateo@gmail.com>
2023-03-09 17:56:39 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,8 @@ int check_prime(int n, int i);
  * @n: the number to be checked
  *
  * Return: integer value
- */int is_prime_number(int n)
+ */
+int is_prime_number(int n)
 {
 	return (check_prime(n, 1));
 }

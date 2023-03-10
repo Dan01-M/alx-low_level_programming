5286e3aeadf2ce1da81b8e0f86bf31db2f428bfc
Dan01-M <www.dan56mateo@gmail.com>
2023-03-09 16:12:03 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,13 @@
  * Return: Nothing.
  */
 void _print_rev_recursion(char *s)
+{
 	if (*s == '\0')
-        {
+	{
 		return;
 	}
-        s++;
+        
+	s++;
 	_print_rev_recursion(s);
 	s--;
 	_putchar(*s);

2f5bc2f550dbf0443b3094f586e968a71ad525eb
Dan01-M <www.dan56mateo@gmail.com>
2024-03-07 17:44:46 -0800

    5-get

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Write a function that returns the nth node of a dlistint.
+ * get_dnodeint_at_index - Write a function that returns the nth nodes.
  * @head: pointer to head of the list
  * @index: index of the node to search for, starting from 0
  * Return: NULL.

